e655ce03f7861a3d3422d3ca61cef148c4e4f6a3
M15 <ismaelmelgar1999@gmail.com>
2021-02-03 14:42:01 +0100

    Creacion del main + arraylist y objetos

--- src/main/java/ismael/tarea6a/Main.java
+++ src/main/java/ismael/tarea6a/Main.java
@@ -17,10 +17,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       ArrayList<Clase_A> clases = new ArrayList<>();
-       
-       
-       
+        ArrayList<Clase_A> clases = new ArrayList<>();
+
+        Clase_A a1 = new Clase_A();
+        Clase_A a2 = new Clase_A();
+        Clase_A a3 = new Clase_B();
+        Clase_A a4 = new Clase_B();
+        Clase_A a5 = new Clase_C();
+        Clase_A a6 = new Clase_C();
+        Clase_A a7 = new Clase_D();
+        Clase_A a8 = new Clase_D();
+
+        clases.add(a1);
+        clases.add(a2);
+        clases.add(a3);
+        clases.add(a4);
+        clases.add(a5);
+        clases.add(a6);
+        clases.add(a7);
+        clases.add(a8);
+
     }
-    
+
 }
