26dd0c2e623ca5a02a3d95782531de7973863268
M15 <ismaelmelgar1999@gmail.com>
2021-02-03 14:41:29 +0100

    Creacion del main + arraylist y objetos

---
+++ src/main/java/ismael/tarea6a/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ismael.tarea6a;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ismael
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ArrayList<Clase_A> clases = new ArrayList<>();
+       
+       
+       
+    }
+    
+}
